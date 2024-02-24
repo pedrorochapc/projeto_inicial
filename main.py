00779d781ce63b87bfee0314ed7a36f068ea4430
Pedro Rocha <pedrorochapc@gmail.com>
2024-02-24 20:55:59 -0300

    salvando o meu arquivo main novamente

---
+++ main.py
@@ -0,0 +1,21 @@
+print("Jornada de dados")
+
+# Variáveis
+idade = 39
+nome = "Pedro"
+
+#Print
+
+print(idade)
+print(nome)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Listas
+lista = [1,2,3,4,5]
+
+# Métodos
+lista_invertida = lista.reverse()
+    
